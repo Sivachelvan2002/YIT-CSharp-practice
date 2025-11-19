7e334e437af54047d14ea84825d778a90a7d4237
sivachelvan2002 <s.sivachelvan2002@gmail.com>
2025-11-19 20:03:16 +0530

    Add initial Windows Forms app for full name display
    Created a new C# Windows Forms application with a form to input first and last names and display the full name. Includes all necessary project files, designer code, resources, and configuration for building and running the application.

---
+++ WindowsFormsApp1/WindowsFormsApp1/fullname.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class fullname
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.btnFullname = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(104, 36);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(100, 20);
+            this.txtFname.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(104, 75);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(100, 20);
+            this.txtLname.TabIndex = 3;
+            // 
+            // btnFullname
+            // 
+            this.btnFullname.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnFullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFullname.Location = new System.Drawing.Point(69, 117);
+            this.btnFullname.Name = "btnFullname";
+            this.btnFullname.Size = new System.Drawing.Size(102, 23);
+            this.btnFullname.TabIndex = 5;
+            this.btnFullname.Text = "Get Full Name";
+            this.btnFullname.UseVisualStyleBackColor = false;
+            this.btnFullname.Click += new System.EventHandler(this.btnFullname_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnFullname);
+            this.groupBox1.Controls.Add(this.txtFname);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtLname);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.groupBox1.Location = new System.Drawing.Point(282, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(238, 195);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Full Name";
+            // 
+            // fullname
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "fullname";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Button btnFullname;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/fullname.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class fullname : Form
+    {
+        public fullname()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("welcome to c#");
+            //String name = txtName.Text;
+           // MessageBox.Show($"Hi {name}");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFullname_Click(object sender, EventArgs e)
+        {
+            String fname=txtFname.Text;
+            String lname=txtLname.Text;
+            
+            MessageBox.Show($"Your Full Name is {fname}{lname}");
+        }
+    }
+}
